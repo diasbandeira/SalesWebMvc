28bd14bf25f3d6c818c894619e0c469cbfaea62b
diasbandeira <diasbandeira@gmail.com>
2022-02-07 15:08:40 -0300

    criaçao do serviço find all sellers

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using SalesWebMvc.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerService;
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+        public IActionResult Index()
+        {
+            List<Seller> sallers = _sellerService.FindAll();
+            
+            return View(sallers);
+        }
+    }
+}
